f7f8d265e789f74df1e22c4f47b2008882d660b8
brrski <115958168+brrski@users.noreply.github.com>
2025-02-11 22:22:34 -0500

    Create day9.py

---
+++ day9.py
@@ -0,0 +1,18 @@
+print("Lets figure out what Generation you belong to!")
+gen = float(input("What is the year you were born in? "))
+if gen >= 2009:
+  print("Hmm, Generation Alpha! Go back to the sandbox!")
+elif gen >= 1996:
+  print("You're a tikTok Gen Z youngin!")
+elif gen >= 1982:
+  print("You are a Millenial!")
+elif gen >= 1965:
+  print("You are a Generation X!")
+elif gen >= 1947:
+  print("You are a Baby Boomer!")
+elif gen >= 1925 :
+  print("You are a Traditionalist!")
+else:
+  print("You are either dead or a time traveller!")
+  
+  
